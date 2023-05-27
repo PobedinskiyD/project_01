1332fb9cb5f1d669afcbd8a0dd1e1328ac16999c
PobedinskiyD <129783776+PobedinskiyD@users.noreply.github.com>
2023-05-27 13:46:46 +0300

    Add files via upload

---
+++ task_2.1.py
@@ -0,0 +1,28 @@
+# Задача 2.1. 
+
+# Создайте две функции maximum и minimum,
+# которые получают список целых чисел в качестве входных данных 
+# и возвращают наибольшее и наименьшее число в этом списке соответственно.
+# Например,
+# * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+# * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+# * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+# * [5]                             -> min = 5, max = 5
+# функции sorted, max и min использовать нельзя!
+
+
+
+def minimum(arr):
+    min_val = arr[0]
+    for i in arr:
+        if i < min_val:
+            min_val = i
+    return min_val
+
+def maximum(arr):
+    max_val = arr[0]
+    for i in arr:
+        if i > max_val:
+            max_val = i
+    return max_val
+
